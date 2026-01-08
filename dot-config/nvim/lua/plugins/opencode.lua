487ef55ab25cfb43e356c35dc07c5a2355f7cedf
Maxime Doury <uncrft@users.noreply.github.com>
2026-01-08 16:00:43 +0100

    refactor(nvim): clean up opencode plugin config

--- dot-config/nvim/lua/plugins/opencode.lua
+++ dot-config/nvim/lua/plugins/opencode.lua
@@ -6,11 +6,8 @@ return {
     },
     config = function()
       ---@type opencode.Opts
-      vim.g.opencode_opts = {
-        -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition".
-      }
+      vim.g.opencode_opts = {}
 
-      -- Required for `opts.auto_reload`.
       vim.o.autoread = true
 
       vim.keymap.set({ "n", "x" }, "<C-a>", function()
@@ -32,53 +29,5 @@ return {
         require("opencode").command("session.half.page.down")
       end, { desc = "opencode half page down" })
     end,
-    -- keys = {
-    --   {
-    --     "<leader>o",
-    --     function()
-    --       require("opencode").ask("@this: ", { submit = true })
-    --     end,
-    --     mode = { "n", "x" },
-    --     desc = "Ask opencode",
-    --   },
-    --   {
-    --     "<leader>O",
-    --     function()
-    --       require("opencode").select()
-    --     end,
-    --     mode = { "n", "x" },
-    --     desc = "Execute opencode action…",
-    --   },
-    --   {
-    --     "ga",
-    --     function()
-    --       require("opencode").prompt("@this")
-    --     end,
-    --     mode = { "n", "x" },
-    --     desc = "Add to opencode",
-    --   },
-    --   {
-    --     "<C-.>",
-    --     function()
-    --       require("opencode").toggle()
-    --     end,
-    --     mode = { "n", "t" },
-    --     desc = "Toggle opencode",
-    --   },
-    --   {
-    --     "<S-C-u>",
-    --     function()
-    --       require("opencode").command("session.half.page.up")
-    --     end,
-    --     desc = "opencode half page up",
-    --   },
-    --   {
-    --     "<S-C-d>",
-    --     function()
-    --       require("opencode").command("session.half.page.down")
-    --     end,
-    --     desc = "opencode half page down",
-    --   },
-    -- },
   },
 }
